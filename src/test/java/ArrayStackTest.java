00a6b3922918c5983defd27bea60e59d359b0d46
JamesHughes89 <jameshughes89@gmail.com>
2021-06-10 12:28:41 -0300

    Add assertj

--- src/test/java/ArrayStackTest.java
+++ src/test/java/ArrayStackTest.java
@@ -1,11 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class ArrayStackTest {
 
     @Test
-    void isEmpty() {
-        assertNull(null);
+    @DisplayName("Pushing a single element on the stack")
+    public void push_pushSingle_oneElementOnStack() {
+        Stack<Integer> stack = new ArrayStack<>();
+        stack.push(99);
+        assertEquals(99, stack.peek());
+        assertEquals(1, stack.size());
     }
+
+    @Test
+    public void expandCapacity_pushSix_callExpandCapacity() {
+    assertj
+
+
+    }
+
+
+
 }
