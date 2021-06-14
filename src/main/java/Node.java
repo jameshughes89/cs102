9098ccacfd4c35d4beca06ef8b70178203805216
JamesHughes89 <jameshughes89@gmail.com>
2021-06-14 10:01:53 -0300

    Write Node class contents

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,2 +1,30 @@
-public class Node {
+public class Node<T> {
+
+    private T data;
+    private Node<T> next;
+
+    public Node() {
+        this(null);
+    }
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 }
