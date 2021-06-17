3424adf1c017e37734f5f767e9a60c4e02aa0bc1
JamesHughes89 <jameshughes89@gmail.com>
2021-06-17 14:34:03 -0300

    Start ArrayQueue

---
+++ src/main/java/ArrayQueue.java
@@ -0,0 +1,30 @@
+public class ArrayQueue<T> implements Queue<T>{
+    @Override
+    public void enqueue(T element) {
+
+    }
+
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    @Override
+    public T first() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    public String toString() {
+
+    }
+}
