13b6c9675c1af5d99ebbaef138643229676461dc
JamesHughes89 <jameshughes89@gmail.com>
2021-06-17 14:39:22 -0300

    Add constructor s

--- src/main/java/ArrayQueue.java
+++ src/main/java/ArrayQueue.java
@@ -1,6 +1,39 @@
 public class ArrayQueue<T> implements Queue<T>{
+
+    private static final int DEFAULT_CAPACITY = 100;
+    private T[] queue;
+    private int front;
+    private int rear;
+    private int size;
+
+    public ArrayQueue() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public ArrayQueue(int initialCapacity) {
+        front = 0;
+        rear = 0;
+        size = 0;
+
+        stack = (T[]) new Object[initialCapacity];
+    }
+
     @Override
     public void enqueue(T element) {
+
+    }
+
+    /**
+     * Doubles the size of the queue array and copy the
+     * contents over.
+     */
+    @SuppressWarnings("unchecked")
+    private void expandCapacity() {
+        T[] newStack = (T[]) new Object[queue.length * 2];
+        for (int i = 0; i < queue.length; ++i) {
+            newStack[i] = queue[i];
+        }
+        queue = newStack;
 
     }
 
