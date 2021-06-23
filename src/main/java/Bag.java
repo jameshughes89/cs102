f0da0ba0d59c7dc6eb9b09ca8d5307be1889d9f9
James Alexander Hughes <jameshughes89@Gmail.com>
2021-06-23 11:09:09 -0300

    :mortar_board: Topic13 Change List Interfaces to Bag Interfaces (#163)

---
+++ src/main/java/Bag.java
@@ -0,0 +1,60 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public interface Bag<T> {
+
+    /**
+     * Add an element to the bag.
+     *
+     * @param element item to be added to the bag
+     */
+    void add(T element);
+
+    /**
+     * Removes and returns a reference to the element equal to the specified element.
+     * If no such element exists a NoSuchElementException is thrown.
+     *
+     * @param element Specified element to find return
+     * @return Element equal to specified element
+     */
+    T remove(T element);
+
+    /**
+     * Determines if a specified element is contained within the bag.
+     *
+     * @param target Element to be searched for
+     * @return True if the element is found, false otherwise
+     */
+    boolean contains(T target);
+
+    /**
+     * Returns the number of occurrences of a specified element in the
+     * bag.
+     *
+     * @param element Element to be counted
+     * @return Number of times the element occured in the bag.
+     */
+    int getCount(T element);
+
+    /**
+     * Tests if the bag is empty and returns a Boolean.
+     *
+     * @return True if the bag is empty (no items), false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the total number of elements in the bag
+     *
+     * @return Number of elements in the bag
+     */
+    int size();
+
+    /**
+     * Returns an iterator object for the elements in the bag in
+     * the proper order for the bag type.
+     *
+     * @return Iterator object for bag instance
+     */
+    Iterator<T> iterator();
+}
---
+++ src/main/java/SortedBag.java
@@ -0,0 +1,45 @@
+public interface SortedBag<T> extends Bag<T> {
+
+    /**
+     * Adds an element to the SortedBag such that the bag remains
+     * sorted.
+     *
+     * @param element Item to be added to the SortedBag
+     */
+    void add(T element);
+
+    /**
+     * Removes and returns a reference to the first element in the bag. If no such element
+     * exists a NoSuchElementException is thrown.
+     *
+     * @return First element in the bag
+     */
+    T removeFirst();
+
+    /**
+     * Removes and returns a reference to the last element in the bag. If no such element
+     * exists a NoSuchElementException is thrown.
+     *
+     * @return Last element in the bag
+     */
+    T removeLast();
+
+    /**
+     * Returns a reference to the first element in the bag. After the method completes,
+     * the item will still be at the front of the bag. If no such element exists, a
+     * NoSuchElementException is thrown.
+     *
+     * @return First element in the bag
+     */
+    T first();
+
+    /**
+     * Returns a reference to the last element in the bag. After the method completes,
+     * the item will still be at the end of the bag. If no such element exists, a
+     * NoSuchElementException is thrown.
+     *
+     * @return Last element in the bag
+     */
+    T last();
+
+}
