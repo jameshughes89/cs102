0c68d741bcfcb0bd5b2556c3c87483120488c0df
JamesHughes89 <jameshughes89@gmail.com>
2021-07-02 13:20:25 -0300

    Add Node class

--- src/main/java/LinkedBinarySearchTree.java
+++ src/main/java/LinkedBinarySearchTree.java
@@ -2,4 +2,57 @@ public class LinkedBinarySearchTree<T> extends BinarySearchTree<T> {
 
     private int size;
     private Node<T> root;
+
+    /**
+     * A Node class for a binary linked tree structure. Each node
+     * contains a reference to data of type T, which may be
+     * null, and a reference to the left and right child
+     * binary linked nodes, which may also be null.
+     * <p>
+     * This class is a static nested class since the node
+     * class is only needed for the implementation of the
+     * LinkedBinarySearchTree.
+     *
+     * @param <T> Type of the data being stored in the node
+     */
+    private static class Node<T> {
+
+        private T data;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node() {
+            this(null);
+        }
+
+        public Node(T data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<T> left) {
+            this.left = left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<T> left) {
+            this.right = right;
+        }
+    }
 }
