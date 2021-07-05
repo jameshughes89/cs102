ea76c8c789a44c96acf971cede39023b6360e06b
JamesHughes89 <jameshughes89@gmail.com>
2021-07-05 16:02:24 -0300

    Add iterator tests

--- src/test/java/LinkedBinarySearchTreeTest.java
+++ src/test/java/LinkedBinarySearchTreeTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -550,5 +551,282 @@ public class LinkedBinarySearchTreeTest {
         assertEquals(12, bst.remove(12));
         assertEquals(62, bst.remove(62));
         assertEquals(87, bst.remove(87));
+    }
+
+    @Test
+    void addSingleElementToLinkedBinarySearchTreeResultsInItBeingTheOnlyElement() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        Iterator<Integer> it = bst.iterator();
+        assertEquals(99, it.next());
+    }
+
+    @Test
+    void addManyElementsToLinkedBinarySearchTreeResultsInElementsBeingPlacedInCorrectPlace() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        Iterator<Integer> it = bst.iterator();
+        assertEquals(12, it.next());
+        assertEquals(25, it.next());
+        assertEquals(37, it.next());
+        assertEquals(50, it.next());
+        assertEquals(62, it.next());
+        assertEquals(75, it.next());
+        assertEquals(87, it.next());
+    }
+
+    @Test
+    void addAndRemoveManyElementsToLinkedBinarySearchTreeResultsInElementsBeingPlacedInCorrectPlace() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        bst.add(87);
+        bst.remove(50);
+        bst.remove(25);
+        bst.add(25);
+        bst.remove(75);
+        bst.remove(37);
+        bst.add(75);
+        bst.add(37);
+        bst.remove(12);
+        bst.remove(62);
+        bst.remove(87);
+        bst.add(12);
+        bst.add(50);
+        bst.add(62);
+        Iterator<Integer> it = bst.iterator();
+        assertEquals(12, it.next());
+        assertEquals(25, it.next());
+        assertEquals(37, it.next());
+        assertEquals(50, it.next());
+        assertEquals(62, it.next());
+        assertEquals(75, it.next());
+        assertEquals(87, it.next());
+    }
+
+    @Test
+    void iteratorHasNextOnEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.iterator();
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void iteratorHasOneElementForLinkedBinarySearchTreeWithOneElement() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        Iterator<Integer> it = bst.iterator();
+        assertEquals(99, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void iteratorNextOnNonEmptyLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        Iterator<Integer> it = bst.iterator();
+        int[] valueOrder = {12, 25, 37, 50, 62, 75, 87};
+        int currentIndex = 0;
+        while (it.hasNext()) {
+            assertEquals(it.next(), valueOrder[currentIndex]);
+            currentIndex++;
+        }
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void iteratorNextOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.iterator();
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void preOrderIteratorHasNextOnEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.preOrderIterator();
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void preOrderIteratorHasOneElementForLinkedBinarySearchTreeWithOneElement() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        Iterator<Integer> it = bst.preOrderIterator();
+        assertEquals(99, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void preOrderIteratorNextOnNonEmptyLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        Iterator<Integer> it = bst.preOrderIterator();
+        int[] valueOrder = {50, 25, 12, 37, 75, 62, 87};
+        int currentIndex = 0;
+        while (it.hasNext()) {
+            assertEquals(it.next(), valueOrder[currentIndex]);
+            currentIndex++;
+        }
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void preOrderIteratorNextOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.preOrderIterator();
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void inOrderIteratorHasNextOnEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.inOrderIterator();
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void inOrderIteratorHasOneElementForLinkedBinarySearchTreeWithOneElement() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        Iterator<Integer> it = bst.inOrderIterator();
+        assertEquals(99, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void inOrderIteratorNextOnNonEmptyLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        Iterator<Integer> it = bst.inOrderIterator();
+        int[] valueOrder = {12, 25, 37, 50, 62, 75, 87};
+        int currentIndex = 0;
+        while (it.hasNext()) {
+            assertEquals(it.next(), valueOrder[currentIndex]);
+            currentIndex++;
+        }
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void inOrderIteratorNextOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.inOrderIterator();
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void postOrderIteratorHasNextOnEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.postOrderIterator();
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void postOrderIteratorHasOneElementForLinkedBinarySearchTreeWithOneElement() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        Iterator<Integer> it = bst.postOrderIterator();
+        assertEquals(99, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void postOrderIteratorNextOnNonEmptyLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        Iterator<Integer> it = bst.postOrderIterator();
+        int[] valueOrder = {12, 37, 25, 62, 87, 75, 50};
+        int currentIndex = 0;
+        while (it.hasNext()) {
+            assertEquals(it.next(), valueOrder[currentIndex]);
+            currentIndex++;
+        }
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void postOrderIteratorNextOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.postOrderIterator();
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void levelOrderIteratorHasNextOnEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.levelOrderIterator();
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void levelOrderIteratorHasOneElementForLinkedBinarySearchTreeWithOneElement() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        Iterator<Integer> it = bst.levelOrderIterator();
+        assertEquals(99, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void levelOrderIteratorNextOnNonEmptyLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        Iterator<Integer> it = bst.levelOrderIterator();
+        int[] valueOrder = {50, 25, 75, 12, 37, 62, 87};
+        int currentIndex = 0;
+        while (it.hasNext()) {
+            assertEquals(it.next(), valueOrder[currentIndex]);
+            currentIndex++;
+        }
+        assertThrows(NoSuchElementException.class, () -> it.next());
+    }
+
+    @Test
+    void levelOrderIteratorNextOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        Iterator<Integer> it = bst.levelOrderIterator();
+        assertThrows(NoSuchElementException.class, () -> it.next());
     }
 }
