f0e2a062d2f9d664a8a02e05674875cd943d2b97
JamesHughes89 <jameshughes89@gmail.com>
2021-07-05 14:49:26 -0300

    Add tests for isEmpty, size, contains, and getCount

--- src/test/java/LinkedBinarySearchTreeTest.java
+++ src/test/java/LinkedBinarySearchTreeTest.java
@@ -5,20 +5,105 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LinkedBinarySearchTreeTest {
 
     @Test
-    void newLinkedBinarySearchTreeIsEmptyReturnsTrue() {
+    void getCountOn
+
+    @Test
+    void isEmptyOnNewLinkedBinarySearchTreeReturnsTrue() {
         BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
         assertTrue(bst.isEmpty());
     }
 
     @Test
-    void emptyLinkedBinarySearchTreeSizeReturnsZero() {
+    void isEmptyOnNonEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        assertFalse(bst.isEmpty());
+    }
+
+    @Test
+    void sizeOfEmptyLinkedBinarySearchTreeReturnsZero() {
         BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
         assertEquals(0, bst.size());
     }
 
     @Test
-    void nonEmptyLinkedBinarySearchTreeIsEmptyReturnsFalse() {
+    void sizeOfBinarySearchTreeWithOneElementReturnsOne() {
         BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
-        assertFalse(bst.isEmpty());
+        bst.add(99);
+        assertEquals(1, bst.size());
+    }
+
+    @Test
+    void sizeOfBinarySearchTreeWithMultipleElementReturnsCorrectSize() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(101);
+        bst.add(11);
+        bst.add(44);
+        assertEquals(4, bst.size());
+    }
+
+    @Test
+    void containsOnEmptyLinkedBinarySearchTreeReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        assertFalse(bst.contains(99));
+    }
+
+    @Test
+    void containsOnLinkedBinarySearchTreeWhenSearchingForNonExistingElementReturnsFalse() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        assertFalse(bst.contains(101));
+    }
+
+    @Test
+    void containsOnLinkedBinarySearchTreeWhenSearchingForSingleEqualElementReturnsTrue() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        assertTrue(bst.contains(99));
+    }
+
+    @Test
+    void containsOnLinkedBinarySearchTreeWhenSearchingForMultipleEqualElementsReturnsTrue() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(99);
+        bst.add(99);
+        assertTrue(bst.contains(99));
+    }
+
+    @Test
+    void getCountOnEmptyLinkedBinarySearchTreeReturnsZero() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        assertEquals(0, bst.getCount(99));
+    }
+
+    @Test
+    void getCountOnLinkedBinarySearchTreeWhenCountingNonExistingElementsReturnsZero() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        assertEquals(0, bst.getCount(101));
+    }
+
+    @Test
+    void getCountOnLinkedBinarySearchTreeWhenCountingSingleEqualElementsReturnsOne() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(101);
+        assertEquals(1, bst.getCount(99));
+    }
+
+    @Test
+    void getCountOnLinkedBinarySearchTreeWhenCountingMultipleEqualElementsReturnsOne() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(101);
+        bst.add(99);
+        bst.add(99);
+        bst.add(99);
+        bst.add(99);
+        assertEquals(5, bst.getCount(99));
     }
 }
