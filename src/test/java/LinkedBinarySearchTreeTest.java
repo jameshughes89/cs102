aa33268c08e040d61a93b4611a39626346b3ad27
JamesHughes89 <jameshughes89@gmail.com>
2021-07-05 15:27:00 -0300

    Add removeMin and removeMax tests

--- src/test/java/LinkedBinarySearchTreeTest.java
+++ src/test/java/LinkedBinarySearchTreeTest.java
@@ -219,4 +219,206 @@ public class LinkedBinarySearchTreeTest {
         bst.max();
         assertTrue(bst.contains(22));
     }
+
+    @Test
+    void removeMinOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        assertThrows(NoSuchElementException.class, () -> bst.removeMin());
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeWhenMinIsRootReturnsCorrectMin() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(101);
+        assertEquals(99, bst.removeMin());
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeWhenMinIsInternalNodeReturnsCorrectMin() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        assertEquals(55, bst.removeMin());
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeWhenMinIsLeafNodeReturnsCorrectMin() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        bst.add(22);
+        assertEquals(22, bst.removeMin());
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeWhenMultipleEqualMinValuesExistReturnsCorrectMin() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        bst.add(22);
+        bst.add(22);
+        bst.add(22);
+        bst.add(22);
+        bst.add(22);
+        assertEquals(22, bst.removeMin());
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeWithSingleMinRemovesElementFromBinarySearchTree() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        bst.add(22);
+        bst.removeMin();
+        assertFalse(bst.contains(22));
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeWithManyMinRemovesOnlyOneElementFromBinarySearchTree() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        bst.add(22);
+        bst.add(22);
+        bst.add(22);
+        bst.removeMin();
+        assertEquals(2, bst.getCount(22));
+    }
+
+    @Test
+    void removeMinOnLinkedBinarySearchTreeUpdatesSize() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        bst.add(22);
+        bst.removeMin();
+        assertEquals(3, bst.size());
+    }
+
+    @Test
+    void multipleRemoveMinCallsOnLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        assertEquals(12, bst.removeMin());
+        assertEquals(25, bst.removeMin());
+        assertEquals(37, bst.removeMin());
+        assertEquals(50, bst.removeMin());
+        assertEquals(62, bst.removeMin());
+        assertEquals(75, bst.removeMin());
+        assertEquals(87, bst.removeMin());
+    }
+
+    @Test
+    void removeMaxOnEmptyLinkedBinarySearchTreeThrowsException() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        assertThrows(NoSuchElementException.class, () -> bst.removeMax());
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeWhenMaxIsRootReturnsCorrectMax() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(101);
+        bst.add(99);
+        assertEquals(101, bst.removeMax());
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeWhenMaxIsInternalNodeReturnsCorrectMax() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(55);
+        bst.add(99);
+        bst.add(77);
+        assertEquals(99, bst.removeMax());
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeWhenMaxIsLeafNodeReturnsCorrectMax() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(22);
+        bst.add(55);
+        bst.add(33);
+        bst.add(99);
+        assertEquals(99, bst.removeMax());
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeWhenMultipleEqualMaxValuesExistReturnsCorrectMax() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(22);
+        bst.add(55);
+        bst.add(33);
+        bst.add(99);
+        bst.add(99);
+        bst.add(99);
+        bst.add(99);
+        bst.add(99);
+        assertEquals(99, bst.removeMax());
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeWithSingleMaxRemovesElementFromBinarySearchTree() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(22);
+        bst.add(55);
+        bst.add(33);
+        bst.add(99);
+        bst.removeMax();
+        assertFalse(bst.contains(99));
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeWithManyMaxRemovesOnlyOneElementFromBinarySearchTree() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(22);
+        bst.add(99);
+        bst.add(55);
+        bst.add(33);
+        bst.add(99);
+        bst.removeMax();
+        assertEquals(2, bst.getCount(99));
+    }
+
+    @Test
+    void removeMaxOnLinkedBinarySearchTreeUpdatesSize() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(99);
+        bst.add(55);
+        bst.add(77);
+        bst.add(22);
+        bst.removeMax();
+        assertEquals(3, bst.size());
+    }
+
+    @Test
+    void multipleRemoveMaxCallsOnLinkedBinarySearchTreeReturnsElementsInCorrectOrder() {
+        BinarySearchTree<Integer> bst = new LinkedBinarySearchTree<>();
+        bst.add(50);
+        bst.add(25);
+        bst.add(75);
+        bst.add(37);
+        bst.add(12);
+        bst.add(62);
+        bst.add(87);
+        assertEquals(87, bst.removeMax());
+        assertEquals(75, bst.removeMax());
+        assertEquals(62, bst.removeMax());
+        assertEquals(50, bst.removeMax());
+        assertEquals(37, bst.removeMax());
+        assertEquals(25, bst.removeMax());
+        assertEquals(12, bst.removeMax());
+    }
 }
