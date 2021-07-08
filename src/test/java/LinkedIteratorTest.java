e2d4ae96ac3d8d832cab026f4545471ea5bfb740
JamesHughes89 <jameshughes89@gmail.com>
2021-07-08 12:39:14 -0300

    Fix method names. Add loops for next calls. Add private method to create linked structure

--- src/test/java/LinkedIteratorTest.java
+++ src/test/java/LinkedIteratorTest.java
@@ -7,33 +7,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedIteratorTest {
 
+    private <T> Node<T> makeLinkedStructure(T t1, T... ts) {
+        Node<T> head = new Node<>(t1);
+        Node<T> current = head;
+        for (int i = 0; i < ts.length; ++i) {
+            Node<T> node = new Node<>(ts[i]);
+            current.setNext(node);
+            current = node;
+        }
+        return head;
+    }
+
     @Test
-    void hasNext_sizeZero_returnsFalse() {
+    void hasNext_sizeZeroLinkedStructure_returnsFalse() {
         Node<Integer> head = null;
         Iterator<Integer> it = new LinkedIterator<>(head);
         assertFalse(it.hasNext());
     }
 
     @Test
-    void hasNext_sizeOne_returnsTrue() {
+    void hasNext_sizeOneLinkedStructure_returnsTrue() {
         Node<Integer> head = new Node<>(10);
         Iterator<Integer> it = new LinkedIterator<>(head);
         assertTrue(it.hasNext());
     }
 
     @Test
-    void hasNext_sizeFive_returnsTrue() {
-        Node<Integer> head = new Node<>(10);
-        head.setNext(new Node<>(11));
-        head.getNext().setNext(new Node<>(12));
-        head.getNext().getNext().setNext(new Node<>(13));
-        head.getNext().getNext().getNext().setNext(new Node<>(14));
+    void hasNext_sizeFiveLinkedStructure_returnsTrue() {
+        Node<Integer> head = makeLinkedStructure(10, 11, 12, 13, 14);
         Iterator<Integer> it = new LinkedIterator<>(head);
         assertTrue(it.hasNext());
     }
 
     @Test
-    void hasNext_sizeOneAfterHasNext_doesNotChangeState() {
+    void hasNext_sizeOneLinkedStructureAfterHasNext_doesNotChangeState() {
         Node<Integer> head = new Node<>(10);
         Iterator<Integer> it = new LinkedIterator<>(head);
         it.hasNext();
@@ -42,24 +49,18 @@ public class LinkedIteratorTest {
     }
 
     @Test
-    void hasNext_sizeFiveAfterFiveHasNext_doesNotChangeState() {
-        Node<Integer> head = new Node<>(10);
-        head.setNext(new Node<>(11));
-        head.getNext().setNext(new Node<>(12));
-        head.getNext().getNext().setNext(new Node<>(13));
-        head.getNext().getNext().getNext().setNext(new Node<>(14));
+    void hasNext_sizeFiveLinkedStructureAfterFiveHasNext_doesNotChangeState() {
+        Node<Integer> head = makeLinkedStructure(10, 11, 12, 13, 14);
         Iterator<Integer> it = new LinkedIterator<>(head);
-        it.hasNext();
-        it.hasNext();
-        it.hasNext();
-        it.hasNext();
-        it.hasNext();
+        for (int i = 0; i < 5; ++i) {
+            it.next();
+        }
         assertTrue(it.hasNext());
         assertEquals(10, it.next());
     }
 
     @Test
-    void hasNext_endOfSizeOne_returnsFalse() {
+    void hasNext_endOfSizeOneLinkedStructure_returnsFalse() {
         Node<Integer> head = new Node<>(10);
         Iterator<Integer> it = new LinkedIterator<>(head);
         it.next();
@@ -67,42 +68,32 @@ public class LinkedIteratorTest {
     }
 
     @Test
-    void hasNext_endOfSizeFive_returnsFalse() {
-        Node<Integer> head = new Node<>(10);
-        head.setNext(new Node<>(11));
-        head.getNext().setNext(new Node<>(12));
-        head.getNext().getNext().setNext(new Node<>(13));
-        head.getNext().getNext().getNext().setNext(new Node<>(14));
+    void hasNext_endOfSizeFiveLinkedStructure_returnsFalse() {
+        Node<Integer> head = makeLinkedStructure(10, 11, 12, 13, 14);
         Iterator<Integer> it = new LinkedIterator<>(head);
-        it.next();
-        it.next();
-        it.next();
-        it.next();
-        it.next();
+        for (int i = 0; i < 5; ++i) {
+            it.next();
+        }
         assertFalse(it.hasNext());
     }
 
     @Test
-    void next_sizeZero_throwsException() {
+    void next_sizeZeroLinkedStructure_throwsException() {
         Node<Integer> head = null;
         Iterator<Integer> it = new LinkedIterator<>(head);
         assertThrows(NoSuchElementException.class, () -> it.next());
     }
 
     @Test
-    void next_sizeOne_returnsElement() {
+    void next_sizeOneLinkedStructure_returnsElement() {
         Node<Integer> head = new Node<>(10);
         Iterator<Integer> it = new LinkedIterator<>(head);
         assertEquals(10, it.next());
     }
 
     @Test
-    void next_sizeFive_returnsElementsInCorrectOrder() {
-        Node<Integer> head = new Node<>(10);
-        head.setNext(new Node<>(11));
-        head.getNext().setNext(new Node<>(12));
-        head.getNext().getNext().setNext(new Node<>(13));
-        head.getNext().getNext().getNext().setNext(new Node<>(14));
+    void next_sizeFiveLinkedStructure_returnsElementsInCorrectOrder() {
+        Node<Integer> head = makeLinkedStructure(10, 11, 12, 13, 14);
         Iterator<Integer> it = new LinkedIterator<>(head);
         assertEquals(10, it.next());
         assertEquals(11, it.next());
@@ -112,18 +103,12 @@ public class LinkedIteratorTest {
     }
 
     @Test
-    void next_endOfSizeFive_throwsException() {
-        Node<Integer> head = new Node<>(10);
-        head.setNext(new Node<>(11));
-        head.getNext().setNext(new Node<>(12));
-        head.getNext().getNext().setNext(new Node<>(13));
-        head.getNext().getNext().getNext().setNext(new Node<>(14));
+    void next_endOfSizeFiveLinkedStructure_throwsException() {
+        Node<Integer> head = makeLinkedStructure(10, 11, 12, 13, 14);
         Iterator<Integer> it = new LinkedIterator<>(head);
-        it.next();
-        it.next();
-        it.next();
-        it.next();
-        it.next();
+        for (int i = 0; i < 5; ++i) {
+            it.next();
+        }
         assertThrows(NoSuchElementException.class, () -> it.next());
     }
 }
