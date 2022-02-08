313d54c5ead6d06d11adaf94d15d3a20816cbe67
James Alexander Hughes <jameshughes89@Gmail.com>
2022-02-08 10:47:05 -0400

    fixes (#442)

--- src/site/data/Assignment1/src/main/java/Asn1.java
+++ src/site/data/Assignment1/src/main/java/Asn1.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Asn1 {
 
-    static final String RELATIVE_RESOURCES = "./src/main/resources/";
+    static final String RELATIVE_RESOURCES = "./src/main/resources/asn1/";
 
     public static void main(String[] args) throws IOException {
 
