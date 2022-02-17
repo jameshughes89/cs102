c81feffc136f9f2af3addd95ebffb1c1b78370c5
James Alexander Hughes <jameshughes89@Gmail.com>
2022-02-17 15:37:58 -0400

    rename DFS

--- src/site/data/Assignment2/test/DfsMazeSolverTest.java
+++ src/site/data/Assignment2/test/DfsMazeSolverTest.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DFSMazeSolverTest {
+public class DfsMazeSolverTest {
 
     private static final String MAZE_NO_SOLUTION =
             "#####\n" +
