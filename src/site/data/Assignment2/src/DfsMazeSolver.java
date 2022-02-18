3d28a558bc159380f70edf7889ddd39e0d9d47a1
James Alexander Hughes <jameshughes89@Gmail.com>
2022-02-18 11:54:58 -0400

    add empty solve

--- src/site/data/Assignment2/src/DfsMazeSolver.java
+++ src/site/data/Assignment2/src/DfsMazeSolver.java
@@ -2,5 +2,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class DfsMazeSolver implements MazeSolver {
-
+    
+    @Override
+    public Set<Cell> solve(Maze maze) {
+        return null;
+    }
 }
