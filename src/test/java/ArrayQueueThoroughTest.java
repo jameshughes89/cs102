0c8d85e4c79609790350dcc399fd2dbac151de22
James Alexander Hughes <jameshughes89@Gmail.com>
2022-02-19 15:41:23 -0400

    include empty tests

--- src/test/java/ArrayQueueThoroughTest.java
+++ src/test/java/ArrayQueueThoroughTest.java
@@ -1,4 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayQueueThoroughTest {
 
@@ -9,5 +15,35 @@ public class ArrayQueueThoroughTest {
     void newQueue() {
         classUnderTest = new ArrayQueue<>();
         preState = new ArrayQueue<>();
+    }
+
+    @Nested
+    class WhenNewEmpty {
+
+        @Test
+        void isEmpty_empty_returnsTrue() {
+            assertTrue(classUnderTest.isEmpty());
+        }
+
+        @Test
+        void size_empty_returnsZero() {
+            assertEquals(0, classUnderTest.size());
+        }
+
+        @Test
+        void first_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.first());
+        }
+
+        @Test
+        void dequeue_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.dequeue());
+        }
+
+        @Test
+        void toString_empty_returnsStringFront() {
+            String expected = "Front --> ";
+            assertEquals(expected, classUnderTest.toString());
+        }
     }
 }
