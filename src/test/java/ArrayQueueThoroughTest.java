558a7a1813fd6e0b5cf1483e7b9f94f9abf43779
James Alexander Hughes <jameshughes89@Gmail.com>
2022-02-19 15:37:22 -0400

    before

--- src/test/java/ArrayQueueThoroughTest.java
+++ src/test/java/ArrayQueueThoroughTest.java
@@ -1,3 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class ArrayQueueThoroughTest {
 
+    private Queue<Integer> classUnderTest;
+    private Queue<Integer> preState;
+
+    @BeforeEach
+    void newQueue() {
+        classUnderTest = new ArrayQueue<>();
+        preState = new ArrayQueue<>();
+    }
 }
