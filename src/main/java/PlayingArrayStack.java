d3d87c15449048b4ea389802599272ebd17b514e
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-08 14:45:51 -0400

    make playing class

---
+++ src/main/java/PlayingArrayStack.java
@@ -0,0 +1,66 @@
+import java.util.NoSuchElementException;
+
+public class PlayingArrayStack {
+    public static void main(String[] args) {
+        // Create an ArrayStack
+        Stack<Integer> myStack = new ArrayStack<>(5);
+
+        // Check stack is empty
+        System.out.println(myStack.size());
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack);
+
+        // Test push
+        myStack.push(0);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        System.out.println(myStack.size());
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack);
+
+        // Test expand capacity
+        myStack.push(10);
+        myStack.push(11);
+        myStack.push(12);
+        myStack.push(13);
+        myStack.push(14);
+        System.out.println(myStack.size());
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack);
+
+        // Test peek
+        System.out.println(myStack.peek());
+        System.out.println(myStack.size());
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack);
+
+        // Test Pop
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.size());
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack);
+
+        // Test peek and pop throwing exception
+        try {
+            myStack.peek();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+        try {
+            myStack.pop();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
+}
