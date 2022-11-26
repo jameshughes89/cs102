75440b400d812cb8338464c39e814493986a902e
James Alexander Hughes <jameshughes89@Gmail.com>
2022-11-26 14:41:37 -0400

    try static again

--- src/test/java/ContactListTest.java
+++ src/test/java/ContactListTest.java
@@ -177,7 +177,7 @@ public class ContactListTest {
             @TestInstance(TestInstance.Lifecycle.PER_CLASS)
             class WhenMany {
 
-                Stream<Arguments> existingFriendsStream() {
+                static Stream<Arguments> existingFriendsStream() {
                     return Stream.of(Arguments.of(EXISTING_FRIEND_FIRST, 0),
                             Arguments.of(EXISTING_FRIEND_MIDDLE, 2),
                             Arguments.of(EXISTING_FRIEND_END, 4));
