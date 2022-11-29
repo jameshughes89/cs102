7797387b3096424e860447c826e962dfcce40548
James Hughes <jameshughes89@gmail.com>
2022-11-29 10:16:44 -0400

    chang equals test to follow contactlist

--- src/test/java/ArrayStackEqualsTest.java
+++ src/test/java/ArrayStackEqualsTest.java
@@ -1,48 +1,63 @@
 import com.google.common.testing.EqualsTester;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ArrayStackEqualsTest {
 
     @Test
     @SuppressWarnings("UnstableApiUsage")
-    @DisplayName("Verify Equals.")
     public void equals_verify_contract() {
-        ArrayStack<Integer> gEmptyDefault = new ArrayStack<>();
-        ArrayStack<Integer> gEmptyCapacity = new ArrayStack<>(10);
-        ArrayStack<Integer> gSingletonDefault = new ArrayStack<>();
-        ArrayStack<Integer> gSingletonCapacity = new ArrayStack<>(10);
-        ArrayStack<Integer> gManyOneDefaultA = new ArrayStack<>();
-        ArrayStack<Integer> gManyOneDefaultB = new ArrayStack<>();
-        ArrayStack<Integer> gManyOneCapacity = new ArrayStack<>(5);
-        ArrayStack<Integer> gManyTwoDefault = new ArrayStack<>();
-        ArrayStack<Integer> gManyTwoCapacity = new ArrayStack<>(10);
-        ArrayStack<Integer> gReverseDefault = new ArrayStack<>();
-        ArrayStack<Integer> gReverseCapacity = new ArrayStack<>(10);
-        ArrayStack<Character> gTypeDefault = new ArrayStack<>();
-        ArrayStack<Character> gTypeCapacity = new ArrayStack<>(10);
+        ArrayStack<Integer> emptyA = new ArrayStack<>();
+        ArrayStack<Integer> emptyB = new ArrayStack<>();
+        ArrayStack<Integer> emptyC = new ArrayStack<>(1000);
 
-        for (int i = 0; i < 6; i++) {
-            gManyOneDefaultA.push(i);
-            gManyOneDefaultB.push(i);
-            gManyOneCapacity.push(i);
-            gManyTwoDefault.push(i * 11);
-            gManyTwoCapacity.push(i * 11);
-            gReverseDefault.push(5 - i);
-            gReverseCapacity.push(5 - i);
-            gTypeDefault.push((char) i);
-            gTypeCapacity.push((char) i);
-        }
-        gSingletonDefault.push(100);
-        gSingletonCapacity.push(100);
+        ArrayStack<Integer> singletonA = new ArrayStack<>();
+        ArrayStack<Integer> singletonB = new ArrayStack<>();
+        ArrayStack<Integer> singletonC = new ArrayStack<>(1000);
+        singletonA.push(10);
+        singletonB.push(10);
+        singletonC.push(10);
+
+        ArrayStack<Integer> manyA = new ArrayStack<>();
+        ArrayStack<Integer> manyB = new ArrayStack<>();
+        ArrayStack<Integer> manyC = new ArrayStack<>(1000);
+        manyA.push(10);
+        manyA.push(20);
+        manyA.push(30);
+        manyB.push(10);
+        manyB.push(20);
+        manyB.push(30);
+        manyC.push(10);
+        manyC.push(20);
+        manyC.push(30);
+
+        ArrayStack<Integer> unequalDifferentValues = new ArrayStack<>();
+        unequalDifferentValues.push(110);
+        unequalDifferentValues.push(120);
+        unequalDifferentValues.push(130);
+
+        ArrayStack<Integer> unequalDifferentOrder = new ArrayStack<>();
+        unequalDifferentOrder.push(30);
+        unequalDifferentOrder.push(20);
+        unequalDifferentOrder.push(10);
+
+        ArrayStack<Integer> unequalDifferentSizes = new ArrayStack<>();
+        unequalDifferentSizes.push(10);
+        unequalDifferentSizes.push(20);
+
+        ArrayStack<Integer> unequalSomeEqual = new ArrayStack<>();
+        unequalSomeEqual.push(20);
+        unequalSomeEqual.push(30);
+        unequalSomeEqual.push(40);
+
 
         new EqualsTester().addEqualityGroup(ArrayStack.class)
-                .addEqualityGroup(gEmptyDefault, gEmptyCapacity)
-                .addEqualityGroup(gSingletonDefault, gSingletonCapacity)
-                .addEqualityGroup(gManyOneDefaultA, gManyOneDefaultB, gManyOneCapacity)
-                .addEqualityGroup(gManyTwoDefault, gManyTwoCapacity)
-                .addEqualityGroup(gReverseDefault, gReverseCapacity)
-                .addEqualityGroup(gTypeDefault, gTypeCapacity)
+                .addEqualityGroup(emptyA, emptyB, emptyC)
+                .addEqualityGroup(singletonA, singletonB, singletonC)
+                .addEqualityGroup(manyA, manyB, manyC)
+                .addEqualityGroup(unequalDifferentValues)
+                .addEqualityGroup(unequalDifferentOrder)
+                .addEqualityGroup(unequalDifferentSizes)
+                .addEqualityGroup(unequalSomeEqual)
                 .testEquals();
     }
 }
