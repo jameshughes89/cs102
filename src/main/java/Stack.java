e3328471dc09e9296cb53d45a50122e843614cde
James Alexander Hughes <jameshughes89@Gmail.com>
2022-11-30 08:57:48 -0400

    make stack consistent with queue

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,16 +1,16 @@
 /**
- * A stack is a linear data structure that has all the action happen from one end referred to as the "top"; all
- * additions (pushes) and removals (pops) happen from the same end, the "top". This data structure has a "last in,
- * first out" (LIFO) property --- the last thing that was added to the stack would be the first thing removed from
- * the stack if a remove were to happen. Adding and removing from anywhere else in the stack is disallowed.
+ * A stack is a linear data structure that has all the action happen from one end referred to as the top; all additions
+ * (pushes) and removals (pops) happen from the same end, the top. Adding and removing from anywhere else in the stack
+ * is disallowed. This data structure has a "last in, first out" (LIFO) property --- the last element that was added to
+ * the stack would be the first element removed from the stack if a remove were to happen.
  *
- * @param <T> Type of elements that are to be on the stack.
+ * @param <T> Type of elements that are to be in the stack.
  */
 public interface Stack<T> {
 
     /**
-     * Adds (pushes) an element to the stack. The push adds the element to the stack such that it becomes the new
-     * "top" of the stack.
+     * Adds (pushes) an element to the stack. The push adds the element to the stack such that it becomes the new top of
+     * the stack.
      *
      * @param element The item to be pushed (added) to the stack.
      * @return True if the element was pushed (added) successfully, false otherwise.
@@ -19,20 +19,20 @@ public interface Stack<T> {
 
     /**
      * Removes (pops) an element from the stack and returns the removed element. The pop removes the element from the
-     * "top" of the stack such that the subsequent element, if it exists, becomes the new "top" of the stack. If no
-     * subsequent element exists, the "top" will be null and the stack will be empty.
+     * top of the stack such that the subsequent element, if it exists, becomes the new top of the stack. If no
+     * subsequent element exists, the top will be null and the stack will be empty.
      *
-     * @return The element on the "top" of the stack.
+     * @return The element at the top of the stack.
      * @throws EmptyCollectionException If removing from an empty stack.
      */
     T pop();
 
     /**
-     * Return the element on the "top" of the stack. Peeking leaves the element on the "top" of the stack and leaves
-     * the collection unchanged.
+     * Return the element at the top of the stack. Calling peek leaves the element at the top of the stack and leaves
+     * the stack unchanged.
      *
-     * @return The element on the "top" of the stack.
-     * @throws EmptyCollectionException If peeking from an empty stack.
+     * @return The element at the top of the stack.
+     * @throws EmptyCollectionException If calling peek on an empty stack.
      */
     T peek();
 
