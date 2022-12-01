51c9ed5b2c4e84bf8b10dda36602666cdf32b574
James Hughes <jameshughes89@gmail.com>
2022-12-01 13:27:15 -0400

    prep tests

--- src/test/java/ArrayQueueTest.java
+++ src/test/java/ArrayQueueTest.java
@@ -1,101 +1,44 @@
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import java.util.NoSuchElementException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArrayQueueTest {
 
-    @Test
-    @DisplayName("A new queue starts empty.")
-    void aNewQueueIsEmpty() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        assertTrue(queue.isEmpty());
+    private ArrayQueue<Integer> classUnderTest;
+    private ArrayQueue<Integer> preState;
+
+    @BeforeEach
+    void createStack() {
+        classUnderTest = new ArrayQueue<>();
+        preState = new ArrayQueue<>();
     }
 
-    @Test
-    @DisplayName("An empty queue has size 0.")
-    void emptyQueueHasSizeZero() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        assertEquals(0, queue.size());
-    }
+    @Nested
+    class WhenNewEmpty {
 
-    @Test
-    @DisplayName("isEmpty return false when it is non empty.")
-    void nonEmptyQueueIsEmptyReturnsFalse() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        queue.enqueue(99);
-        assertFalse(queue.isEmpty());
-    }
+        @Nested
+        class WhenSingleton {
 
-    @Test
-    @DisplayName("Enqueuing items updates the size of the queue.")
-    void enqueuingUpdatesSize() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        queue.enqueue(99);
-        queue.enqueue(101);
-        assertEquals(2, queue.size());
-    }
+            @Nested
+            @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+            class WhenMany {
 
-    @Test
-    @DisplayName("Enqueuing one item results in it being the first.")
-    void enqueuingOneItemIsFirst() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        queue.enqueue(99);
-        assertEquals(99, queue.first());
-    }
-
-    @Test
-    @DisplayName("Enqueuing successfully returns true.")
-    void enqueuingSuccessfullyReturnsTrue() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        assertTrue(queue.enqueue(99));
-
-    }
-
-    @Test
-    @DisplayName("Enqueueing and Dequeuing returns in FIFO order.")
-    void enqueueingAndDequeuingReturnsElementsInFIFOOrder() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        for (int i = 0; i < 6; ++i) {
-            queue.enqueue(i);
+            }
         }
-        for (int i = 0; i < 6; ++i) {
-            assertEquals(i, queue.dequeue());
-        }
+
     }
 
-    @Test
-    @DisplayName("Enqueuing 6 elements expands capacity while maintaining queues FIFO ordering.")
-    void enqueuingBeyondCapacityCallsExpandCapacityToMakeRoomWhileMaintainingQueueOrdering() {
-        Queue<Integer> queue = new ArrayQueue<>(5);
-        queue.enqueue(99);
-        queue.dequeue();
-        // front = rear = 1
-        for (int i = 0; i < 6; ++i) {
-            queue.enqueue(i);
+    @Nested
+    class WhenLarge {
+        @Test
+        void push_large_successfullyExpandsCapacity() {
+            for (int i = 0; i < 1000; i++) {
+                classUnderTest.enqueue(i);
+            }
+            assertEquals(1000, classUnderTest.size());
         }
-        // After 6 enqueues, expandCapacity is called.
-        // Ensure first was not overwritten by 6th
-        // consecutive enqueue and whole queue is in
-        // proper FIFO order with 6th element at the rear
-        for (int i = 0; i < 6; ++i) {
-            assertEquals(i, queue.dequeue());
-        }
-    }
-
-    @Test
-    @DisplayName("Dequeuing throws NoSuchElementException when queue is empty.")
-    void dequeueEmptyQueueThrowsException() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        assertThrows(NoSuchElementException.class, () -> queue.dequeue());
-    }
-
-    @Test
-    @DisplayName("First throws NoSuchElementException when queue is empty.")
-    void firstEmptyQueueThrowsException() {
-        Queue<Integer> queue = new ArrayQueue<>();
-        assertThrows(NoSuchElementException.class, () -> queue.first());
     }
 }
