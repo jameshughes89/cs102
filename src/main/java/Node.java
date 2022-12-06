c921b070d7a3163c6916c82122de6a05a4ba640e
James Hughes <jameshughes89@gmail.com>
2022-12-06 13:25:03 -0400

    copy from linkedStack class

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,10 +1,8 @@
 /**
- * A Node class for a singly linked structure. Each node
- * contains a reference to data of type T, which may be
- * null, and a reference to the next/subsequent/successor
- * singly linked node, which may also be null.
+ * A Node class for a singly linked structure. Each node contains a nullable reference to data of type T, and a
+ * reference to the next/subsequent/successor singly linked node, which may be a null reference.
  *
- * @param <T> Type of the data being stored in the node
+ * @param <T> Type of the data being stored in the node.
  */
 public class Node<T> {
 
