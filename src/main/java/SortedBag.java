e1fe5edfcca0e32c0d906605c4ea8ccc606233b4
James Hughes <jameshughes89@gmail.com>
2022-12-07 13:32:59 -0400

    clean sorted bag interface

--- src/main/java/SortedBag.java
+++ src/main/java/SortedBag.java
@@ -1,44 +1,52 @@
-public interface SortedBag<T> extends Bag<T> {
+/**
+ * A SortedBag is a Bag where the elements are kept in some defined ordering. A SortedBag has an order defined by the
+ * characteristics of the type of elements within the SortedBag; the elements themselves determine the ordering. All
+ * modifications to the SortedBag's contents are done in such a way that the ordering is preserved.
+ *
+ * @param <T> Type of elements that are to be in the SortedBag. These elements must have a defined ordering.
+ */
+public interface SortedBag<T extends Comparable<? super T>> extends Bag<T> {
 
     /**
-     * Adds an element to the SortedBag such that the bag remains
-     * sorted.
+     * Adds the specified element to the SortedBag such that the SortedBag remains sorted.
      *
-     * @param element Item to be added to the SortedBag
+     * @param element Element to be added to the SortedBag.
+     * @return True if the element was added successfully, false otherwise.
      */
-    void add(T element);
+    @Override
+    boolean add(T element);
 
     /**
-     * Removes and returns a reference to the first element in the bag. If no such element
-     * exists a NoSuchElementException is thrown.
+     * Removes the first (lowest) element from the SortedBag. The remove is done in such a way that the order of the
+     * elements within the SortedBag is preserved. Returns the element that was removed from the SortedBag.
      *
-     * @return First element in the bag
+     * @return The first (lowest) element currently in the SortedBag.
+     * @throws EmptyCollectionException If the SortedBag is empty.
      */
     T removeFirst();
 
     /**
-     * Removes and returns a reference to the last element in the bag. If no such element
-     * exists a NoSuchElementException is thrown.
+     * Removes the last (highest) element from the SortedBag. The remove is done in such a way that the order of the
+     * elements within the SortedBag is preserved. Returns the element that was removed from the SortedBag.
      *
-     * @return Last element in the bag
+     * @return The last (highest) element currently in the SortedBag.
+     * @throws EmptyCollectionException If the SortedBag is empty.
      */
     T removeLast();
 
     /**
-     * Returns a reference to the first element in the bag. After the method completes,
-     * the item will still be at the front of the bag. If no such element exists, a
-     * NoSuchElementException is thrown.
+     * Returns the first (lowest) element currently in the SortedBag.
      *
-     * @return First element in the bag
+     * @return The first (lowest) element currently in the SortedBag.
+     * @throws EmptyCollectionException If the SortedBag is empty.
      */
     T first();
 
     /**
-     * Returns a reference to the last element in the bag. After the method completes,
-     * the item will still be at the end of the bag. If no such element exists, a
-     * NoSuchElementException is thrown.
+     * Returns the last (highest) element currently in the SortedBag.
      *
-     * @return Last element in the bag
+     * @return The last (highest) element currently in the SortedBag.
+     * @throws EmptyCollectionException If the SortedBag is empty.
      */
     T last();
 }
