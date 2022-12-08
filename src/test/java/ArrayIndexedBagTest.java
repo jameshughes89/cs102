461162386050fb35b658ed7ef878285102ab447b
James Hughes <jameshughes89@gmail.com>
2022-12-08 15:30:45 -0400

    singleton tests

--- src/test/java/ArrayIndexedBagTest.java
+++ src/test/java/ArrayIndexedBagTest.java
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayIndexedBagTest {
 
@@ -19,6 +21,101 @@ public class ArrayIndexedBagTest {
 
     @Nested
     class WhenNewEmpty {
+
+        @Test
+        void add_elementArgument_returnsTrue() {
+            assertTrue(classUnderTest.add(0));
+        }
+
+        @Test
+        void add_validIndex_returnsTrue() {
+            assertTrue(classUnderTest.add(0, 0));
+        }
+
+        @Test
+        void add_negativeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.add(-1, 0));
+        }
+
+        @Test
+        void add_tooLargeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.add(1, 0));
+        }
+
+        @Test
+        void set_negativeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.set(-1, 0));
+        }
+
+        @Test
+        void set_tooLargeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.set(0, 0));
+        }
+
+        @Test
+        void get_negativeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.get(-1));
+        }
+
+        @Test
+        void get_tooLargeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.get(0));
+        }
+
+        @Test
+        void remove_negativeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.remove(-1));
+        }
+
+        @Test
+        void remove_tooLargeIndex_throwsIndexOutOfBoundsException() {
+            assertThrows(IndexOutOfBoundsException.class, () -> classUnderTest.remove(0));
+        }
+
+        @Test
+        void remove_elementArgument_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.remove(Integer.valueOf(0)));
+        }
+
+        @Test
+        void indexOf_nonexistentElement_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.indexOf(0));
+        }
+
+        @Test
+        void contains_nonexistentElement_returnsFalse() {
+            assertFalse(classUnderTest.contains(0));
+        }
+
+        @Test
+        void count_nonexistentElement_returnsZero() {
+            assertEquals(0, classUnderTest.count(0));
+        }
+
+        @Test
+        void isEmpty_empty_returnsTrue() {
+            assertTrue(classUnderTest.isEmpty());
+        }
+
+        @Test
+        void size_empty_returnsZero() {
+            assertEquals(0, classUnderTest.size());
+        }
+
+        @Test
+        void iterator_empty_noNext() {
+            assertFalse(classUnderTest.iterator().hasNext());
+        }
+
+        @Test
+        void iterator_empty_nextThrowsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.iterator().next());
+        }
+
+        @Test
+        void toString_empty_returnsEmptyString() {
+            assertEquals("", classUnderTest.toString());
+        }
 
         @Nested
         class WhenSingleton {
