80ce4b86a8d1b0c4e6f8943288beeb988b615b82
James Alexander Hughes <jameshughes89@Gmail.com>
2022-12-21 14:12:48 -0400

    empty tests

--- src/test/java/ArraySortedBagTest.java
+++ src/test/java/ArraySortedBagTest.java
@@ -2,12 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +17,72 @@ public class ArraySortedBagTest {
 
     @Nested
     class WhenNewEmpty {
+
+        @Test
+        void add_successfulAdd_returnsTrue() {
+            assertTrue(classUnderTest.add(0));
+        }
+
+        @Test
+        void remove_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.remove(0));
+        }
+
+        @Test
+        void removeFirst_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.removeFirst());
+        }
+
+        @Test
+        void removeLast_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.removeLast());
+        }
+
+        @Test
+        void first_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.first());
+        }
+
+        @Test
+        void last_empty_throwsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.last());
+        }
+
+        @Test
+        void contains_nonexistentElement_returnsFalse() {
+            assertFalse(classUnderTest.contains(0));
+        }
+
+        @Test
+        void count_nonexistentElement_returnsZero() {
+            assertEquals(0, classUnderTest.count(0));
+        }
+
+        @Test
+        void isEmpty_empty_returnsTrue() {
+            assertTrue(classUnderTest.isEmpty());
+        }
+
+        @Test
+        void size_empty_returnsZero() {
+            assertEquals(0, classUnderTest.size());
+        }
+
+        @Test
+        void iterator_empty_noNext() {
+            assertFalse(classUnderTest.iterator().hasNext());
+        }
+
+        @Test
+        void iterator_empty_nextThrowsNoSuchElementException() {
+            assertThrows(NoSuchElementException.class, () -> classUnderTest.iterator().next());
+        }
+
+        @Test
+        void toString_empty_returnsEmptyString() {
+            assertEquals("", classUnderTest.toString());
+        }
+
 
         @Nested
         class WhenSingleton {
