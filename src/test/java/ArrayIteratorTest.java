925c02f00214f5903e22844cc5495e76777848bb
James Hughes <jameshughes89@gmail.com>
2023-01-25 09:52:30 -0400

    rename methods and singleton tests

--- src/test/java/ArrayIteratorTest.java
+++ src/test/java/ArrayIteratorTest.java
@@ -11,37 +11,37 @@ public class ArrayIteratorTest {
     class WhenEmpty {
 
         @Test
-        void hasNext_emptyCapacityZero_returnsFalse() {
+        void hasNext_capacityZero_returnsFalse() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{}, 0);
             assertFalse(iterator.hasNext());
         }
 
         @Test
-        void hasNext_emptyCapacityOne_returnsFalse() {
+        void hasNext_capacityOne_returnsFalse() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{null}, 0);
             assertFalse(iterator.hasNext());
         }
 
         @Test
-        void hasNext_emptyCapacityMany_returnsFalse() {
+        void hasNext_capacityMany_returnsFalse() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{null, null, null, null, null}, 0);
             assertFalse(iterator.hasNext());
         }
 
         @Test
-        void next_emptyCapacityZero_throwsNoSuchElementException() {
+        void next_capacityZero_throwsNoSuchElementException() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{}, 0);
             assertThrows(NoSuchElementException.class, () -> iterator.next());
         }
 
         @Test
-        void next_emptyCapacityOne_throwsNoSuchElementException() {
+        void next_capacityOne_throwsNoSuchElementException() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{null}, 0);
             assertThrows(NoSuchElementException.class, () -> iterator.next());
         }
 
         @Test
-        void next_emptyCapacityMany_throwsNoSuchElementException() {
+        void next_capacityMany_throwsNoSuchElementException() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{null, null, null, null, null}, 0);
             assertThrows(NoSuchElementException.class, () -> iterator.next());
         }
@@ -51,26 +51,52 @@ public class ArrayIteratorTest {
     class WhenSingleton {
 
         @Test
-        void hasNext_singleton_returnsTrue() {
+        void hasNext_capacityOne_returnsTrue() {
+            ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10}, 1);
+            assertTrue(iterator.hasNext());
+        }
+
+        @Test
+        void hasNext_capacityMany_returnsTrue() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10, null, null, null, null}, 1);
             assertTrue(iterator.hasNext());
         }
 
         @Test
-        void next_singleton_returnsElement() {
+        void next_capacityOne_returnsElement() {
+            ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10}, 1);
+            assertEquals(10, iterator.next());
+        }
+
+        @Test
+        void next_capacityMany_returnsElement() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10, null, null, null, null}, 1);
             assertEquals(10, iterator.next());
         }
 
         @Test
-        void hasNext_postNext_returnsFalse() {
+        void hasNext_postNextCapacityOne_returnsFalse() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10, null, null, null, null}, 1);
             iterator.next();
             assertFalse(iterator.hasNext());
         }
 
         @Test
-        void next_postNext_throwsNoSuchElementException() {
+        void hasNext_postNextCapacityMany_returnsFalse() {
+            ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10}, 1);
+            iterator.next();
+            assertFalse(iterator.hasNext());
+        }
+
+        @Test
+        void next_postNextCapacityOne_throwsNoSuchElementException() {
+            ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10}, 1);
+            iterator.next();
+            assertThrows(NoSuchElementException.class, () -> iterator.next());
+        }
+
+        @Test
+        void next_postNextCapacityMany_throwsNoSuchElementException() {
             ArrayIterator<Integer> iterator = new ArrayIterator<>(new Integer[]{10, null, null, null, null}, 1);
             iterator.next();
             assertThrows(NoSuchElementException.class, () -> iterator.next());
