82814ace6a009a291f5eba2b3f5b1c6b27761b77
James Hughes <jameshughes89@gmail.com>
2023-02-02 12:51:20 -0400

    clean recursive linear tests

--- src/test/java/SearchingMethodsTest.java
+++ src/test/java/SearchingMethodsTest.java
@@ -50,62 +50,42 @@ public class SearchingMethodsTest {
         assertEquals(4, SearchingMethods.iterativeLinearSearch(DUPLICATE_NEEDLE, UNSORTED_ARRAY));
     }
 
-
     @Test
     void recursiveLinearSearch_nullHaystack_throwsNullPointerException() {
-        Integer[] myHaystack = null;
-        Integer myNeedle = 99;
-        assertThrows(NullPointerException.class, () -> SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
+        assertThrows(NullPointerException.class, () -> SearchingMethods.recursiveLinearSearch(99, null, 0));
+    }
+
+    @Test
+    void recursiveLinearSearch_nullNeedle_returnsSentinel() {
+        assertEquals(NOT_FOUND, SearchingMethods.recursiveLinearSearch(null, UNSORTED_ARRAY, 0));
     }
 
     @Test
     void recursiveLinearSearch_mismatchTypes_returnsSentinel() {
-        Integer[] myHaystack = new Integer[10];
-        String myNeedle = "99";
-        assertEquals(-1, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
+        assertEquals(NOT_FOUND, SearchingMethods.recursiveLinearSearch("99", UNSORTED_ARRAY, 0));
     }
 
     @Test
     void recursiveLinearSearch_sizeZeroHaystack_returnsSentinel() {
-        Integer[] myHaystack = new Integer[0];
-        Integer myNeedle = 99;
-        assertEquals(-1, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
+        assertEquals(NOT_FOUND, SearchingMethods.recursiveLinearSearch(99, new Integer[0], 0));
     }
 
     @Test
     void recursiveLinearSearch_needleNotWithinHaystack_returnsSentinel() {
-        Integer[] myHaystack = {0, 4, 8, 12, 18, 25, 31, 33, 36, 35, 37, 40, 40, 40, 49, 55, 61, 62, 63, 74, 81, 89, 90, 98};
-        Integer myNeedle = 99;
-        assertEquals(-1, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
+        assertEquals(NOT_FOUND, SearchingMethods.recursiveLinearSearch(NOT_WITHIN_HAYSTACK, UNSORTED_ARRAY, 0));
     }
 
-    @Test
-    void recursiveLinearSearch_needleAtZeroIndex_returnsCorrectIndex() {
-        Integer[] myHaystack = {0, 4, 8, 12, 18, 25, 31, 33, 36, 35, 37, 40, 40, 40, 49, 55, 61, 62, 63, 74, 81, 89, 90, 98};
-        Integer myNeedle = 0;
-        assertEquals(0, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
-    }
-
-    @Test
-    void recursiveLinearSearch_needleAtEndOfHaystack_returnsCorrectIndex() {
-        Integer[] myHaystack = {0, 4, 8, 12, 18, 25, 31, 33, 36, 35, 37, 40, 40, 40, 49, 55, 61, 62, 63, 74, 81, 89, 90, 98};
-        Integer myNeedle = 98;
-        assertEquals(23, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
-    }
-
-    @Test
-    void recursiveLinearSearch_needleAtMiddleOfHaystack_returnsCorrectIndex() {
-        Integer[] myHaystack = {0, 4, 8, 12, 18, 25, 31, 33, 36, 35, 37, 40, 40, 40, 49, 55, 61, 62, 63, 74, 81, 89, 90, 98};
-        Integer myNeedle = 55;
-        assertEquals(15, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
+    @ParameterizedTest
+    @CsvSource({"33,0", "0,14", "90,23"})
+    void recursiveLinearSearch_needleAtZeroIndex_returnsCorrectIndex(Integer needle, int index) {
+        assertEquals(index, SearchingMethods.recursiveLinearSearch(needle, UNSORTED_ARRAY, 0));
     }
 
     @Test
     void recursiveLinearSearch_multipleEqualNeedles_returnsFirstOccurrenceIndex() {
-        Integer[] myHaystack = {0, 4, 8, 12, 18, 25, 31, 33, 36, 35, 37, 40, 40, 40, 49, 55, 61, 62, 63, 74, 81, 89, 90, 98};
-        Integer myNeedle = 40;
-        assertEquals(11, SearchingMethods.recursiveLinearSearch(myNeedle, myHaystack, 0));
+        assertEquals(4, SearchingMethods.recursiveLinearSearch(DUPLICATE_NEEDLE, UNSORTED_ARRAY, 0));
     }
+
 
     @Test
     void iterativeBinarySearch_nullHaystack_throwsNullPointerException() {
