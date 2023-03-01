bf70951e6893722a6812d840ad652c1a17217209
James Alexander Hughes <jameshughes89@Gmail.com>
2023-03-01 10:13:40 -0400

    update handout files (#867)

--- src/site/assignments/huffman-codes/Assignment4/src/HuffmanNodeComparator.java
+++ src/site/assignments/huffman-codes/Assignment4/src/HuffmanNodeComparator.java
@@ -1,6 +1,20 @@
 import java.util.Comparator;
 
 public class HuffmanNodeComparator implements Comparator<HuffmanNode> {
+
+    /**
+     * Compares two HuffmanNode objects. HuffmanNode objects are ordered based on the following rules:
+     * <p>
+     * 1. A HuffmanNode with a smaller weight comes first
+     * 2. If a HuffmanLeaf and a HuffmanInternal have the same weight, the leaf comes first
+     * 3. If two HuffmanLeaf nodes have the same weight, the one with the character with the smaller ASCII value
+     * comes first
+     * 4. If two HuffmanInternal nodes are compared, their order does not matter
+     *
+     * @param huff1 the first object to be compared
+     * @param huff2 the second object to be compared
+     * @return negative int if huff1 < huff 2, 0 if huff1 == huff2, and positive int if huff1 > huff 2
+     */
     public int compare(HuffmanNode huff1, HuffmanNode huff2) {
         return 0;
     }
