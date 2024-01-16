bda4f283a1f748b96477fb0209545f56dd17bb60
James Hughes <jameshughes89@gmail.com>
2024-01-16 15:50:07 -0400

    linked stack

--- src/test/java/LinkedStackEqualsTest.java
+++ src/test/java/LinkedStackEqualsTest.java
@@ -1,49 +1,27 @@
 import com.google.common.testing.EqualsTester;
 import org.junit.jupiter.api.Test;
+import playground.LinkedStack;
 
 class LinkedStackEqualsTest {
 
     @Test
     @SuppressWarnings("UnstableApiUsage")
     public void equals_verify_contract() {
-        LinkedStack<Integer> emptyA = new LinkedStack<>();
-        LinkedStack<Integer> emptyB = new LinkedStack<>();
+        LinkedStack<Integer> emptyA = of();
+        LinkedStack<Integer> emptyB = of();
 
-        LinkedStack<Integer> singletonA = new LinkedStack<>();
-        LinkedStack<Integer> singletonB = new LinkedStack<>();
-        singletonA.push(10);
-        singletonB.push(10);
+        LinkedStack<Integer> singletonA = of(10);
+        LinkedStack<Integer> singletonB = of(10);
 
+        LinkedStack<Integer> manyA = of(10, 20, 30);
+        LinkedStack<Integer> manyB = of(10, 20, 30);
 
-        LinkedStack<Integer> manyA = new LinkedStack<>();
-        LinkedStack<Integer> manyB = new LinkedStack<>();
-        manyA.push(10);
-        manyA.push(20);
-        manyA.push(30);
-        manyB.push(10);
-        manyB.push(20);
-        manyB.push(30);
+        LinkedStack<Integer> unequalDifferentValues = of(110, 120, 130);
+        LinkedStack<Integer> unequalDifferentOrder = of(30, 20, 10);
+        LinkedStack<Integer> unequalDifferentSizes = of(10, 20);
+        LinkedStack<Integer> unequalSomeEqual = of(20, 30, 40);
 
-        LinkedStack<Integer> unequalDifferentValues = new LinkedStack<>();
-        unequalDifferentValues.push(110);
-        unequalDifferentValues.push(120);
-        unequalDifferentValues.push(130);
-
-        LinkedStack<Integer> unequalDifferentOrder = new LinkedStack<>();
-        unequalDifferentOrder.push(30);
-        unequalDifferentOrder.push(20);
-        unequalDifferentOrder.push(10);
-
-        LinkedStack<Integer> unequalDifferentSizes = new LinkedStack<>();
-        unequalDifferentSizes.push(10);
-        unequalDifferentSizes.push(20);
-
-        LinkedStack<Integer> unequalSomeEqual = new LinkedStack<>();
-        unequalSomeEqual.push(20);
-        unequalSomeEqual.push(30);
-        unequalSomeEqual.push(40);
-
-        new EqualsTester().addEqualityGroup(ArrayStack.class)
+        new EqualsTester().addEqualityGroup(LinkedStack.class)
                 .addEqualityGroup(emptyA, emptyB)
                 .addEqualityGroup(singletonA, singletonB)
                 .addEqualityGroup(manyA, manyB)
@@ -52,5 +30,13 @@ class LinkedStackEqualsTest {
                 .addEqualityGroup(unequalDifferentSizes)
                 .addEqualityGroup(unequalSomeEqual)
                 .testEquals();
+    }
+
+    private <T> LinkedStack<T> of(T... ts) {
+        LinkedStack<T> stack = new LinkedStack<>();
+        for (T element : ts) {
+            stack.push(element);
+        }
+        return stack;
     }
 }
