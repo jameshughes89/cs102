28b927895d5a7ebcc149b793ddb0507e5d10058c
James Hughes <jameshughes89@gmail.com>
2024-01-26 13:28:09 -0400

    remove final

--- src/main/java/ContactList.java
+++ src/main/java/ContactList.java
@@ -230,7 +230,7 @@ public class ContactList {
 
     // [start-hashCode]
     @Override
-    public final int hashCode() {
+    public int hashCode() {
         int result = Objects.hash(this.size);
         for (int i = 0; i < this.size(); i++) {
             result = result * 97 + Objects.hashCode(this.friends[i]);
